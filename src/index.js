a1991eb9e3654be9291c577bd7e950bc731d79cc
Haley Sorensen <sorensenhaley@gmail.com>
2024-08-23 13:55:25 -0600

    feat: Fetch weather api and convert to JSON object

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import _ from 'lodash';
 import './style.css';
+
+async function getData(){
+    const weatherURL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=A6M43K4MK42TMSVWHGFK3F4X8';
+    const response = await fetch(weatherURL, {mode: 'cors'});
+    const data = await response.json();
+    return data;
+}
+const weatherData = await getData();
+console.log(weatherData)
